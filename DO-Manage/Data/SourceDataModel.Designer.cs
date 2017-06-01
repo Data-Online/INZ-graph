8f54bdaa4933240fb211306cca039e267ab24a5e
Data-Online <graeme@dataonline.co.nz>
2017-06-01 13:00:14 +1200

    Update application name. Database first configuration

--- DO-Manage/Data/SourceDataModel.Designer.cs
+++ DO-Manage/Data/SourceDataModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\atkin\Source\Repos\GraphTest\Graph-ASPNET-46-Snippets\namespace DO_Manage\Data\SourceDataModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\atkin\Source\Repos\INZ-graph\DO-Manage\Data\SourceDataModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
